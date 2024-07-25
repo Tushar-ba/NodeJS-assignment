c72e38e7cb9082897565fdede744b327764e79e0
Tushar <tusharba321@gmail.com>
2024-07-25 12:48:23 +0530

    added registration and token generation for user

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
+import nodemailer from 'nodemailer'
+
+
+const registerUser = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Please provide all required fields' })
+  }
+
+  try {
+    const userExists = await User.findOne({ email })
+    if (userExists) return res.status(400).json({ message: 'User already exists' })
+
+    const user = await User.create({ email, password })
+
+    
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    })
+    
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: 'Email Verification',
+      text: `Please verify your email by clicking the following link: ${process.env.BASE_URL}/verify/${generateToken(user._id)}`
+    }
+
+    await transporter.sendMail(mailOptions)
+    
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      balance: user.balance,
+      token: generateToken(user._id)
+    })
+  } catch (error) {
+    console.error('Error:', error)  
+    res.status(500).json({ message: error.message })
+  }
+}
+
+export { registerUser }
--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-
-connectDB()
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
 app.use(express.json())
+app.use('/api/users', userRoutes)
 
-app.get('/',(req,res)=>{
-    res.send('api is running')
-})
-
-const PORT = process.env.PORT
-app.listen(PORT,(req,res)=>{
-    console.log("the servere is running")
-})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
