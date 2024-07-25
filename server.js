191eace7a3627e5cb38d86091d308b7ba175e8d6
Tushar <tusharba321@gmail.com>
2024-07-25 11:11:19 +0530

    Created server logic

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('api is running')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT,(req,res)=>{
+    console.log("the servere is running")
+})
